a2849785a9c85edd36ca5dc101f6bcc91e7037a3
Reinaldo Junio Dias de Abreu <reinaldodiasabreu@gmail.com>
2020-08-10 13:57:08 -0300

    bug fixed: adding new day of the week and time even with an empty field

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,14 +1,32 @@
 // Procurar o botão, e quando clicado clona o campo add time
 document.querySelector("#add-time").addEventListener('click', cloneField);
 
-function cloneField() {
-    // Clona o nodo
-    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
-    // Limpa os campos input
+function checkField() {
+    const newFieldContainer = document.querySelector(".schedule-item");
+    const select = newFieldContainer.querySelectorAll('select');
     const fields = newFieldContainer.querySelectorAll('input');
-    fields.forEach(function(element){
-        element.value = "";
-    });
-    // Adiciona o clone como filho
-    document.querySelector("#schedule-items").appendChild(newFieldContainer);
+    let voidElements = true
+    if(select[0].value == ""){
+        voidElements = false
+    }
+    for(i=0; i<2; i++){
+        if(fields[i].value == ""){
+            voidElements = false
+        }
+    }
+    return voidElements;
+}
+
+function cloneField() {
+    if( checkField() ){
+        // Clona o nodo
+        const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+        // Limpa os campos input
+        const fields = newFieldContainer.querySelectorAll('input');
+        fields.forEach(function(element){
+            element.value = "";
+        });
+        // Adiciona o clone como filho
+        document.querySelector("#schedule-items").appendChild(newFieldContainer);
+    }
 }
